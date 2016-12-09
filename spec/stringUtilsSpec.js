4773b45c9b0a68241502d1aacb6d25faa898eab4
Andriy Fetsyuk <AndreusAnd@gmail.com>
2016-12-09 16:52:40 +0200

    added basic functionality and tests

---
+++ spec/stringUtilsSpec.js
@@ -0,0 +1,13 @@
+(function () {
+    describe('stringUtils tests', function () {
+        var stringUtils = require('../string-helpers.js').stringUtils;
+
+        it('Should trim slash from left', function () {
+            expect(stringUtils.trimLeft('/some-res/', '/')).toBe('some-res/');
+        });
+
+        it('Should trim slashes from left', function () {
+            expect(stringUtils.trimLeft('///some-res/', '/')).toBe('some-res/');
+        });
+    });
+})();
--- string-helpers.js
+++ string-helpers.js
@@ -1,5 +1,53 @@
-exports.stringUtils = {
-    trimLeft: function (str, trimChars) {
-        throw Error('Not yet implemented');
+(function () {
+    /**
+     * @member stringUtils
+     * Contains string util functions 
+     */
+    var stringUtils = {};
+
+    stringUtils.trimLeft = trimLeft;
+
+    exports.stringUtils = stringUtils;
+
+    /**
+     * @function 
+     * 
+     * Trims input source string from left by character specified
+     * @param {string} source input source sting
+     * @param trimCHars {string} trim characters. Any of these characters will be removed from left side of input string
+     */
+    function trimLeft(source, trimChars) {
+        var trimCharsHashTable = string2hashtable(trimChars),
+            result = source.substr(0);
+
+        while (containsAny(trimCharsHashTable, result[0])) {
+            result = result.substr(1);
+        }
+
+        return result;
     }
-}
+
+    /**
+     * @function containsAny
+     * 
+     * @param sourceHashTable {object}
+     * @param char {string}
+     */
+    function containsAny(sourceHashTable, char) {
+        return !!sourceHashTable[char];
+    }
+
+    /**
+     * @function string2hashtable
+     * 
+     * @param source {string}
+     */
+    function string2hashtable(source) {
+        var res = {};
+        for (var i = 0; i < source.length; i++) {
+            res[source[i]] = true;
+        }
+
+        return res;
+    }
+})();
